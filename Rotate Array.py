1bd78ade234bd3ff16536f8efadcb2648a119dbf
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-01-10 19:58:14 +0530

    Update Rotate array.py

--- Rotate Array.py
+++ Rotate Array.py
@@ -28,20 +28,6 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        """
-        nums = collections.deque(nums)
 
-        nums.rotate(k)
-
-        nums = list(nums)
-        print(nums)
-        
-        """
-        """
-        for i in range(k):
-            nums=nums[-1:]+nums[:-1]
-        print nums
-        
-        """
-        for i in range(k):
+        for i in range(k % len(nums)):
             nums.insert(0,nums.pop())
