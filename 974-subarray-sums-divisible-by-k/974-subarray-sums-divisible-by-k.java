73456427e7c58663b5a571ca34e3f9bc80c0c3d4
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-07-23 10:49:17 +0530

    Time: 23 ms (71.65%), Space: 46.4 MB (95.00%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
@@ -14,9 +14,13 @@ class Solution {
 //             // if sum<0 eg -2 and k=5 then -2%5=0 
 //             // but we want 3 there. 
 //             // So add k to make sum becomes positive
+            
 //             if(rem<0)
 //                 rem += k;
             
+            // ---- One line for the above ---- //
+            // as the sum can be negative,
+            // taking modulo twice
             int rem = ((sum % k) + k) % k;
             
             if(map.containsKey(rem)){
