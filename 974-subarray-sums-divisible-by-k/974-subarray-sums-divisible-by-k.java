1a7736c0a641e2d89cb1fadf40cdd836af8fafa2
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-07-23 10:37:37 +0530

    Time: 44 ms (20.81%), Space: 63.2 MB (24.70%) - LeetHub

---
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        
+        int ans = 0;
+        int sum = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        
+        for(int i:nums){
+            sum += i; 
+            int rem = sum%k;
+            
+            // if sum<0 eg -2 and k=5 then -2%5=0 
+            // but we want 3 there. 
+            // So add k to make sum becomes positive
+            if(rem<0)
+                rem += k;
+            
+            if(map.containsKey(rem)){
+                ans += map.get(rem);
+            }
+            
+            map.put(rem, map.getOrDefault(rem,0)+1);
+            
+            
+            
+        }
+        return ans;
+    }
+}
