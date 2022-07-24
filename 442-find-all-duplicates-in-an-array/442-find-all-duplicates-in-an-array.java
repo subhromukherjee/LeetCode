bde1b2442331f72a7fa3afb81f5c1c12786480e0
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-07-24 12:58:14 +0530

    Time: 11 ms (44.97%), Space: 68.3 MB (25.73%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -1,16 +1,28 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        int no_of_chars = 1000000;
-        int[] count = new int[no_of_chars];
-        List<Integer> ans = new ArrayList<>();
-        for(int i:nums)
-            count[i]++;
+//         int no_of_chars = 1000000;
+//         int[] count = new int[no_of_chars];
+//         List<Integer> ans = new ArrayList<>();
+//         for(int i:nums)
+//             count[i]++;
         
-        for(int i=1;i<=nums.length;i++)
-            if(count[i]>1){
-                ans.add(i);
-            }
+//         for(int i=1;i<=nums.length;i++)
+//             if(count[i]>1){
+//                 ans.add(i);
+//             }
                 
+//         return ans;
+        // General approach : count each char
+        //     But in this qstn nums range is given so we can convert already seen numbers into negative
+        List<Integer> ans = new ArrayList<>();
+        for(int i:nums){
+            int index = Math.abs(i);
+            if(nums[index-1]>0)
+                nums[index-1] *= -1;
+            else
+                ans.add(index);
+        }
         return ans;
+        
     }
 }
