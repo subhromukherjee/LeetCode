83f3e65dfb956a2c01b8ce5279c300ded0c6a40a
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-07-24 11:51:53 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (86.76%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,12 +1,26 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int index = 0;
-        Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[strs.length-1];
-        // Can use startsWith function also
-        while(index<first.length() && first.charAt(index)==last.charAt(index))
-            index++;
-        return index==0 ? "":first.substring(0,index);
+        // int index = 0;
+        // Arrays.sort(strs);
+        // String first = strs[0];
+        // String last = strs[strs.length-1];
+        // // Can use startsWith function also
+        // while(index<first.length() && first.charAt(index)==last.charAt(index))
+        //     index++;
+        // return index==0 ? "":first.substring(0,index);
+        // T.C. nLogn cz of sorting
+        
+        String prefix = strs[0];
+         /*
+            * We check if the prefix is at the beginning of the currentWord, 
+                if it is not the "indexOf" will return a "-1"
+            * if it return a "-1" then we reduce the last character of the prefix by one position and that                  will be our new prefix.
+            * We will iterate over this until the indexOf returns a "0" which means we have a match, or we                  have removed all the characters of the prefix string, 
+                which will be a "" (empty string)*/
+        for(String s:strs){
+            while(s.indexOf(prefix)!=0)
+                prefix = prefix.substring(0, prefix.length()-1);
+        }
+        return prefix;
     }
 }
