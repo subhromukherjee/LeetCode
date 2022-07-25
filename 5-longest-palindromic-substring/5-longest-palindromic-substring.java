0af3937f5e53c193cc4b1bf5d7ea688dc0bb28e3
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-07-25 23:19:09 +0530

    Time: 687 ms (12.26%), Space: 87.1 MB (18.41%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        
+        boolean[][] dp=new boolean[s.length()][s.length()];
+        int start=0, end=1, len=0;
+        for(int g=0; g<s.length();g++){
+            
+            // Start point of every diagonal is 0,gap
+            // Ends at last column
+            for(int i=0, j=g; j<s.length(); i++,j++){
+                // 1 char is always palindrome
+                if(g == 0){
+                    dp[i][j] = true;
+                }
+                else if(g == 1){
+                    if(s.charAt(i) == s.charAt(j))
+                        dp[i][j] = true;
+                    else
+                        dp[i][j] = false;
+                }
+                else {
+                    // Extremeties should be same and inner part should be palindrome
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true)
+                        dp[i][j] = true;
+                    else
+                        dp[i][j] = false;
+                    
+                }
+                if(dp[i][j]){
+                    len = g+1;
+                    start = i;
+                    end = j;
+                    
+                }
+                    
+                
+            }
+        }
+        // Length of longest plaindromic substring
+        // System.out.println(len);
+        return s.substring(start, end+1);
+        
+    }
+}
