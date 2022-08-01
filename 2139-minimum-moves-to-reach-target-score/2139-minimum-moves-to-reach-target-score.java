65801010ad441b49a69b9e38b3f832fbe1d57dc7
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-08-01 23:20:52 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (33.89%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -2,6 +2,7 @@ class Solution {
     public int minMoves(int target, int maxDoubles) {
         // Greedy
         int moves = 0;
+        // Start from target and check if it is divisible by 2 if so divide and reduce the maxdoubles .
        while(target>0 && maxDoubles>0){
            if(target%2 == 0){
                target/=2;
@@ -9,10 +10,9 @@ class Solution {
            }else{
                target--;
            }
-           moves++;
-               
-               
+           moves++;                    
        }
+        // After finishing doubles
         if(target!=1)
             moves += target-1;
         return moves;
