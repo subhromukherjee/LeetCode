d2d81086f3c1529cd8696ed8cca159df5d83cb04
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-08-03 22:06:50 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (49.70%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -1,13 +1,29 @@
 class Solution {
     public int countOperations(int num1, int num2) {
-        int count=0;
-        while(num1 != 0 && num2!=0){
-            if(num1>=num2)
-                num1 -= num2;
-            else
-                num2 -= num1;
-            count++;
+        // int count=0;
+        // while(num1 != 0 && num2!=0){
+        //     if(num1>=num2)
+        //         num1 -= num2;
+        //     else
+        //         num2 -= num1;
+        //     count++;
+        // }
+        // return count;
+         int cnt = 0;
+        while (Math.min(num1, num2) > 0) {
+            if (num1 < num2) {
+                int tmp = num1;
+                num1 = num2;
+                num2 = tmp;
+            }
+            cnt += num1 / num2;
+            num1 %= num2;
         }
-        return count;
+        return cnt;
+        // Euclidean algorithm
+//          Locate the smaller and the bigger from num1 and num2;
+//          bigger / smaller is the number of operations we need to accumulate;
+//          bigger % smaller is the new value of the bigger;
+//          repeat till one becomes 0.
     }
 }
