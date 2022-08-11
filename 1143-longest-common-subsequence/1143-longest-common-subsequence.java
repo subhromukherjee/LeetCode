fb41a62a720f5b1b5f02201b702f6eecb8456ad2
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-08-11 22:58:14 +0530

    Time: 82 ms (5.24%), Space: 46.2 MB (73.55%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int[][] dp = new int[1001][1001];
+    public int longestCommonSubsequence(String text1, String text2) {
+        //https://youtu.be/4Urd0a0BNng
+        // Memoized solution
+        for(int rows[]: dp)
+            Arrays.fill(rows,-1);
+        return lcs(text1, text2, text1.length(), text2.length());
+    }
+    
+    int lcs(String x, String y, int m, int n){
+        // Base case
+        // Think of smallest valid input
+        // It will x="" ans y=""
+        // In that case there is nothing common
+        // Ex: x="abc" y="" -> No common -> lcs length=0
+        if(m==0 || n==0)    return 0;
+        if(dp[m-1][n-1] != -1)    return dp[m-1][n-1];
+        // If last element in common
+        if(x.charAt(m-1) == y.charAt(n-1))
+            return dp[m-1][n-1]=1+lcs(x, y, m-1, n-1);    // Found 1 common -> so increase length by 1
+        else
+            return dp[m-1][n-1]=Math.max(lcs(x,y,m,n-1), lcs(x,y,m-1,n));   
+                // We have 2 choices here
+                // Either take reduce 1st string or reduce 2nd
+                // take whichever gives max result
+        
+    }
+}
