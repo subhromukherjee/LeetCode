17f342442b88e3ca8e950f6ccea76af0070c62c2
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-08-12 11:40:19 +0530

    Time: 23 ms (69.68%), Space: 60.8 MB (83.30%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -4,11 +4,16 @@ class Solution {
         int minDiff = Integer.MAX_VALUE;
         List<List<Integer>> res = new ArrayList<>();
         for(int i=1;i<arr.length;i++){
-            minDiff = Math.min(minDiff, arr[i]-arr[i-1]);
-        }
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]-arr[i-1] == minDiff)
+            int diff = arr[i]-arr[i-1];
+            if(diff<minDiff){
+                // Now that we get a new diff, remove all the values stored for the previous diffs
+                // Store the pairs with the current min diff
+                res.clear();
                 res.add(Arrays.asList(arr[i-1],arr[i]));
+                minDiff = arr[i]-arr[i-1];
+            }else if(diff == minDiff){
+                res.add(Arrays.asList(arr[i-1],arr[i]));
+            }
         }
         return res;
     }
