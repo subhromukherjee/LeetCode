78ba6a6ace691ff64d35264aec9833917500ba57
Subhro Mukherjee <62748308+subhromukherjee@users.noreply.github.com>
2022-08-14 21:33:56 +0530

    Time: 7 ms (17.54%), Space: 44.2 MB (58.74%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,20 +1,37 @@
 class Solution {
     // Nearest smaller to the right
-    public int[] finalPrices(int[] prices) {
-        int n = prices.length;
-        int[] res = new int[n];
-        int i,j;
-        for(i=0;i<n;i++){
-            for(j=i+1;j<n;j++){
-                if(prices[j]<=prices[i]){
-                    res[i]=prices[i]-prices[j];
-                    break;
-                }
+    // Brute force
+//     public int[] finalPrices(int[] prices) {
+//         int n = prices.length;
+//         int[] res = new int[n];
+//         int i,j;
+//         for(i=0;i<n;i++){
+//             for(j=i+1;j<n;j++){
+//                 if(prices[j]<=prices[i]){
+//                     res[i]=prices[i]-prices[j];
+//                     break;
+//                 }
                     
-            }
-            // Smaller element not found -> loop ran totally
-            if(j==n)    res[i]=prices[i];
+//             }
+//             // Smaller element not found -> loop ran totally
+//             if(j==n)    res[i]=prices[i];
+//         }
+//         return res;
+//     }
+    public int[] finalPrices(int[] prices){
+        int n = prices.length;
+        Stack<Integer> stack = new Stack<>();
+        int res[] = new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!stack.empty() && stack.peek()>prices[i])
+                stack.pop();
+            if(!stack.isEmpty())
+                res[i] = prices[i] - stack.peek();
+            else
+                res[i] = prices[i];
+            stack.push(prices[i]);
         }
         return res;
     }
+    
 }
